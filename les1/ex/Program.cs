037ab82143bb7a6697d709ee9275e7c6c48a7bf8
anonim <djumppp@mail.ru>
2023-06-19 14:12:51 +0300

    home work

--- les1/ex/Program.cs
+++ les1/ex/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int number = ReadInt("enter three-digit number: ");
+int amount = number.ToString().Length;
+
+if (amount < 3 && amount > 3)
+{
+	Console.WriteLine("wrong number entered");
+}
+else
+{
+	Console.WriteLine(InCenter(number));
+}
+int ReadInt(string message)
+{
+ Console.Write(message);
+ return Convert.ToInt32(Console.ReadLine()!);
+}
+int InCenter(int a)
+{  
+	int result = ((a / 10) % 10);
+	return result;
+}
---
+++ les1/ex3/Program.cs
@@ -0,0 +1,34 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int dayNumber = ReadInt("Enter number from 1 to 7: ");
+Console.WriteLine(WorkHoliday(dayNumber));
+
+int ReadInt(string message)
+{
+ Console.Write(message);
+ return Convert.ToInt32(Console.ReadLine());
+}
+
+string WorkHoliday(int num)
+{
+	if (num > 0 && num < 8)
+  {
+ 		if (num == 7 || num == 6)
+    {
+ 			Console.Write( num + " - holiday");
+    }
+ 		else
+    {
+ 			Console.Write( num + " - work");
+    }
+  }
+ 	else
+  {
+ 		Console.Write("enter a number from 1 to 7");
+  }
+ 	return " day.";
+}
